e75d672d7ea011f5effc93fa54178d09c9278e4f
ysfsyln22 <ysfsyln22@gmail.com>
2026-06-18 12:23:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PROJE22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter("D:\\Sample.txt");
+                sw.WriteLine("Hello World!!");
+                sw.WriteLine("From the StreamWriter class");
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("İşlem Tamamlandı.");
+            }
+        }
+    }
+}
